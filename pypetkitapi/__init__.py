6a787e5bd0599ee2f39ec5b78263a7ba53419723
L. <rankjie@gmail.com>
2026-02-18 23:23:57 +0200

    Bump version 1.3.0.dev7 → 1.3.0.dev8

--- pypetkitapi/__init__.py
+++ pypetkitapi/__init__.py
@@ -55,7 +55,7 @@ from .media import DownloadDecryptMedia, MediaCloud, MediaFile, MediaManager
 from .purifier_container import Purifier
 from .water_fountain_container import WaterFountain
 
-__version__ = "1.3.0.dev7"
+__version__ = "1.3.0.dev8"
 
 __all__ = [
     "CTW3",
